ee81c37208df7885f2ce619b3f4709190a3f63e6
Korosenpaii <marlcorpuz19@gmail.com>
2024-04-23 23:11:27 +0800

    updated sql

--- hireme.sql
+++ hireme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 22, 2024 at 06:50 PM
+-- Generation Time: Apr 23, 2024 at 11:11 PM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -33,11 +33,11 @@ DROP TABLE IF EXISTS `companies`;
 CREATE TABLE IF NOT EXISTS `companies` (
   `CompanyID` int NOT NULL AUTO_INCREMENT,
   `UserID` int NOT NULL,
-  `CompanyName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `CompanyDescription` text COLLATE utf8mb4_general_ci NOT NULL,
-  `LegalDocuments` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'redundant, will remove later',
-  `CompanyAddress` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `VerificationStatus` enum('Verified','Pending','Rejected') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pending',
+  `CompanyName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `CompanyDescription` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `LegalDocuments` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'redundant, will remove later',
+  `CompanyAddress` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `VerificationStatus` enum('Verified','Pending','Rejected') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pending',
   PRIMARY KEY (`CompanyID`),
   KEY `UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -52,10 +52,10 @@ DROP TABLE IF EXISTS `companyapplication`;
 CREATE TABLE IF NOT EXISTS `companyapplication` (
   `CompanyApplicationID` int NOT NULL AUTO_INCREMENT,
   `CompanyID` int NOT NULL,
-  `DocumentName` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `DocumentFilePath` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `VerificationStatus` enum('Pending','Verified','Rejected') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pending',
-  `ReasonForRejection` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `DocumentName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `DocumentFilePath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `VerificationStatus` enum('Pending','Verified','Rejected') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'Pending',
+  `ReasonForRejection` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `Date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`CompanyApplicationID`),
   KEY `CompanyID` (`CompanyID`)
@@ -106,18 +106,18 @@ DROP TABLE IF EXISTS `jobs`;
 CREATE TABLE IF NOT EXISTS `jobs` (
   `JobID` int NOT NULL AUTO_INCREMENT,
   `CompanyID` int NOT NULL,
-  `JobTitle` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `JobDescription` text COLLATE utf8mb4_general_ci NOT NULL,
-  `JobType` enum('Full-Time','Part-Time','Contract','Intern') COLLATE utf8mb4_general_ci NOT NULL,
+  `JobTitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `JobDescription` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `JobType` enum('Full-Time','Part-Time','Contract','Intern') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `SalaryMin` decimal(10,2) NOT NULL,
   `SalaryMax` decimal(10,2) NOT NULL,
-  `WorkHours` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `JobLocation` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `JobLocationType` enum('WFH','On Site') COLLATE utf8mb4_general_ci NOT NULL,
+  `WorkHours` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `JobLocation` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `JobLocationType` enum('WFH','On Site') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `VerificationStatus` enum('Verified','Pending','Rejected') COLLATE utf8mb4_general_ci NOT NULL,
-  `JobIndustry` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `OtherIndustry` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `VerificationStatus` enum('Verified','Pending','Rejected') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `JobIndustry` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `OtherIndustry` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`JobID`),
   KEY `CompanyID` (`CompanyID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -133,9 +133,9 @@ CREATE TABLE IF NOT EXISTS `jobseekerapplication` (
   `JobSeekerApplicationID` int NOT NULL AUTO_INCREMENT,
   `JobID` int DEFAULT NULL,
   `UserID` int DEFAULT NULL,
-  `ResumeFilePath` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `ResumeFilePath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `ApplicationDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Status` enum('Pending','Rejected','Verified') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `Status` enum('Pending','Rejected','Verified') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   PRIMARY KEY (`JobSeekerApplicationID`),
   KEY `JobID` (`JobID`),
   KEY `UserID` (`UserID`)
@@ -151,11 +151,11 @@ DROP TABLE IF EXISTS `jobseekers`;
 CREATE TABLE IF NOT EXISTS `jobseekers` (
   `JobSeekerID` int NOT NULL AUTO_INCREMENT,
   `UserID` int NOT NULL,
-  `FirstName` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
-  `LastName` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `FirstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `LastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `BirthDate` date NOT NULL,
-  `Address` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `ContactNumber` varchar(11) COLLATE utf8mb4_general_ci NOT NULL,
+  `Address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `ContactNumber` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`JobSeekerID`),
   KEY `UserID` (`UserID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -169,11 +169,11 @@ CREATE TABLE IF NOT EXISTS `jobseekers` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `UserID` int NOT NULL AUTO_INCREMENT,
-  `Username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `Password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `Email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `Email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `Role` enum('Admin','Company','Job Seeker','User','Manager') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'User',
-  `Token` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `Token` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `unique_email` (`Email`),
   UNIQUE KEY `unique_username` (`Username`)
